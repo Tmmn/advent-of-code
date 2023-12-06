0e7c03bfcc947dd50ddcf45dbe216246a4010799
Timon Leupp <timon.leupp@uzh.ch>
2023-12-06 18:57:47 +0100

    optimized day 6️⃣ Solution for Part 2️⃣ now takes 1️⃣0️⃣ microseconds ⏲️

--- 2023/06/06.py
+++ 2023/06/06.py
@@ -1,5 +1,5 @@
 import time
-
+import math
 
 class BoatRace:
     def __init__(self, races):
@@ -32,6 +32,29 @@ class BoatRace:
                 winning_button_times.append(button_pressed)
         return len(winning_button_times)
 
+    def part1_optimized(self):
+        winning_button_product = 1
+        for i, race_time in enumerate(self.race_stats1[0]):
+            root = (race_time**2 - 4*self.race_stats1[1][i])**0.5
+            lower_hold_time = math.ceil((race_time - root)/2)
+            lower_hold_time = lower_hold_time + 1 \
+                if float(lower_hold_time) == (race_time - root)/2 else lower_hold_time
+            upper_hold_time = math.floor((race_time + root - 0.1)/2)
+            upper_hold_time = upper_hold_time - 1 \
+                if float(upper_hold_time) == (race_time + root)/2 else upper_hold_time
+            winning_button_product *= upper_hold_time - lower_hold_time + 1
+        return winning_button_product
+
+    def part2_optimized(self):
+        root = (self.race_stats2[0] ** 2 - 4 * self.race_stats2[1]) ** (1 / 2)
+        lower_hold_time = math.ceil((self.race_stats2[0] - root) / 2)
+        lower_hold_time = lower_hold_time \
+            if float(lower_hold_time) == (self.race_stats2[0] - root) / 2 else lower_hold_time
+        upper_hold_time = math.floor((self.race_stats2[0] + root) / 2)
+        upper_hold_time = upper_hold_time - 1 \
+            if float(upper_hold_time) == (self.race_stats2[0] + root) / 2 else upper_hold_time
+        return upper_hold_time - lower_hold_time + 1
+
 
 if __name__ == "__main__":
     with open('06.txt', 'r') as source:
@@ -44,9 +67,19 @@ if __name__ == "__main__":
     for entry in result1:
         product1 *= entry
     end_time1 = time.perf_counter_ns()
-    print(f"Part 1: result is {product1} (in {end_time1 - start_time1}ns)")
+    print(f"Part 1: result is {product1} (in {(1e-3 * (end_time1 - start_time1)):.3f}μs)")
 
     start_time2 = time.perf_counter()
     result2 = boat_race.part2_wining_time()
     end_time2 = time.perf_counter()
-    print(f"Part 2: result is {result2} (in {end_time2-start_time2}s)")
+    print(f"Part 2: result is {result2} (in {(end_time2 - start_time2):.3f}s)")
+
+    opt_start_time1 = time.perf_counter_ns()
+    opt_result1 = boat_race.part1_optimized()
+    opt_end_time1 = time.perf_counter_ns()
+    print(f"OPTIMIZED Part 1: result is {opt_result1} (in {(1e-3 * (opt_end_time1 - opt_start_time1)):.3f}μs)")
+
+    opt_start_time2 = time.perf_counter_ns()
+    opt_result2 = boat_race.part2_optimized()
+    opt_end_time2 = time.perf_counter_ns()
+    print(f"OPTIMIZED Part 2: result is {opt_result2} (in {(1e-3 * (opt_end_time2 - opt_start_time2)):.3f}μs)")

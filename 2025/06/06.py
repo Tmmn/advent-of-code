8c1d7a657ed41e68111f4f9a7213952ecdc83725
Timon Leupp <timon@leupp.net>
2025-12-08 12:13:37 +0100

    solution day 06 part 02 👨🏼‍🔧

--- 2025/06/06.py
+++ 2025/06/06.py
@@ -5,6 +5,7 @@ class Crusher:
 
     def __init__(self, math):
         self.math = [line.split() for line in math]
+        self.math2 = [list(line) for line in math]
 
     @runner
     def part_1(self):
@@ -12,6 +13,22 @@ class Crusher:
         answer = 0
         for i in range(len(self.math[0])):
             answer += self.calc([int(line[i]) for line in self.math[:-1]], instructions[i])
+        return answer
+
+    @runner
+    def part_2(self):
+        instructions = self.math[-1]
+        num_count = len(instructions) - 1
+        answer = 0
+        numbers = []
+        for i in range(len(self.math2[0]) - 1, -1, -1):
+            if all([line[i] == " " for line in self.math2[:-1]]):
+                answer += self.calc(numbers, instructions[num_count])
+                num_count -= 1
+                numbers = []
+            else:
+                numbers.append(int("".join(line[i] for line in self.math2[:-1])))
+        answer += self.calc(numbers, instructions[num_count])
         return answer
 
     @staticmethod
@@ -26,3 +43,4 @@ if __name__ == "__main__":
         lines = [row[:-1] for row in source.readlines()]
     crusher = Crusher(lines)
     crusher.part_1()
+    crusher.part_2()
